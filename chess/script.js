bc175f9d264dc4c3dc1d737a91681cef03adfccf
arxs-abd <70242781+arxs-abd@users.noreply.github.com>
2022-12-12 04:49:40 +0800

    just update chess

--- chess/script.js
+++ chess/script.js
@@ -2,14 +2,14 @@ const containerBoard = document.querySelector('.container-board')
 const html = document.querySelector('html')
 
 // Note
-// Baru bisa pindahkan Pionnya, masih banyak lagi
+// Baru bisa pindahkan Suda Bisa Pindahkan Bishop
 const army = {
-    rook : '♜',
-    knight : '♞',
-    king : '♚',
-    queen : '♛',
-    bishop : '♝',
-    pawn : '♟',
+    rook : '♜', // Done
+    knight : '♞', // Done
+    king : '♚', // Done
+    queen : '♛', // Done
+    bishop : '♝', // Done
+    pawn : '♟', // Done
 }
 
 const position = [
@@ -35,12 +35,13 @@ for (let i = 0; i < 8; i++) {
         box.classList.add('box')
         box.dataset.x = i
         box.dataset.y = j
-        // box.draggable = true
 
         const data = {
             bord : null,
             player : null,
-            box : box
+            box : box,
+            x : i,
+            y : j
         }
 
         box.dataset.board = boxColor % 2
@@ -49,7 +50,8 @@ for (let i = 0; i < 8; i++) {
             box.dataset.player = i === 0 ? 0 : 1
             data.bord = position[0][j]
             data.player = i === 0 ? 0 : 1
-        } else if (i === 1 || i === 6) {
+        } 
+        else if (i === 1 || i === 6) {
             box.innerText = army['pawn']
             data.bord = 'pawn'
             box.dataset.player = i === 1 ? 0 : 1
@@ -63,16 +65,16 @@ for (let i = 0; i < 8; i++) {
             const y = Number(box.dataset.y)
 
             if (checkNextMove(x, y)) {
-                // console.log('nextMove')
                 removeSelected()
                 move(x, y)
 
                 nextMove = []
                 gameTurn = (gameTurn === 1) ? 0 : 1
-                html.style.animation = (gameTurn === 1 ? 'black-white' : 'white-black') + ' 500ms'
-                html.style.backgroundColor = gameTurn === 1 ? 'white' : '#222831'
+                html.style.animation = (gameTurn === 1 ? 'black-white' : 'white-black') + ' 1000ms'
+                html.style.backgroundColor = gameTurn === 1 ? '#FFFDD2' : '#222831'
             } else {
                 removeSelected()
+                // console.log(game[x][y])
     
                 if (!this.innerText) return
 
@@ -92,8 +94,6 @@ for (let i = 0; i < 8; i++) {
     containerBoard.appendChild(row)
 }
 
-// console.log('White Turn')
-
 function removeSelected() {
     for (let i = 0; i < 8; i++) {
         for (let j = 0; j < 8; j++) {
@@ -101,7 +101,7 @@ function removeSelected() {
             if (box.classList.contains('selected')) {
                 box.classList.remove('selected')
             }
-            if (box.dataset.dot === '1') box.dataset.dot = 0
+            if (box.dataset.dot !== '0') box.dataset.dot = 0
         }
     }
 }
@@ -145,7 +145,29 @@ function checkMove(x, y) {
                         y : y,
                     })
                 }
-
+                
+                
+            }
+            // check Kill
+            // Check if stil in board
+            if (x - 1 >= 0 && y - 1 >= 0) {
+                if (game[x - 1][y - 1].player !== game[x][y].player && game[x - 1][y - 1].player !== null) {
+                    game[x - 1][y - 1].box.dataset.dot = 2
+                    nextMove.push({
+                        x : x - 1,
+                        y : y - 1,
+                    })
+                }
+            }
+            // Check if stil in board
+            if (x - 1 >= 0 && y + 1 < 8) {
+                if (game[x - 1][y + 1].player !== game[x][y].player && game[x - 1][y + 1].player !== null) {
+                    game[x - 1][y + 1].box.dataset.dot = 2
+                    nextMove.push({
+                        x : x - 1,
+                        y : y + 1,
+                    })
+                }
             }
           
         // For Player 0 
@@ -170,6 +192,7 @@ function checkMove(x, y) {
                 }
             // Pawn 1 Steps
             } else {
+                
                 if (!game[x + 1][y].bord) {
                     game[x + 1][y].box.dataset.dot = 1
                     nextMove.push({
@@ -177,9 +200,92 @@ function checkMove(x, y) {
                         y : y,
                     })
                 }
+
+            }
+            // check Kill
+            // Check if stil in board
+            if (x + 1 < 8 && y + 1 < 8) {
+
+                if (game[x + 1][y + 1].player !== game[x][y].player && game[x + 1][y + 1].player !== null) {
+                    game[x + 1][y + 1].box.dataset.dot = 2
+                    nextMove.push({
+                        x : x + 1,
+                        y : y + 1,
+                    })
+                }
+            }
+
+            // Check if stil in board
+            if (x + 1 < 8 && y - 1 >= 0) {
+                if (game[x + 1][y - 1].player !== game[x][y].player && game[x + 1][y - 1].player !== null) {
+                    game[x + 1][y - 1].box.dataset.dot = 2
+                    nextMove.push({
+                        x : x + 1,
+                        y : y - 1,
+                    })
+                }
             }
         }
 
+    } else if (data.bord === 'knight') {
+            const knightMove = [
+                [x - 1, y - 2], [x - 1, y + 2], [x - 2, y - 1], [x - 2, y + 1],
+                [x + 1, y - 2], [x + 1, y + 2], [x + 2, y + 1], [x + 2, y - 1]
+            ]
+
+            knightMove.forEach(function(move) {
+                const i = move[0]
+                const j = move[1]
+                if (i >= 0 && i < 8 && j >= 0 && j < 8) {
+                    if (!game[i][j].bord) {
+                        game[i][j].box.dataset.dot = 1
+                        nextMove.push({
+                            x : i,
+                            y : j,
+                         })
+                    } else {
+                        if (game[i][j].player !== game[x][y].player && game[i][j].player !== null) {
+                            game[i][j].box.dataset.dot = 2
+                            nextMove.push({
+                                x : i,
+                                y : j,
+                            })
+                        }
+                    }
+                }
+
+            })
+
+    } else if (data.bord === 'bishop') {
+        bishopMove(x, y)
+    } else if (data.bord === 'rook') {
+        rookMove(x, y)
+    } else if (data.bord === 'queen') {
+        rookMove(x, y)
+        bishopMove(x, y)
+    } else if (data.bord === 'king') {
+        for (let i = -1; i < 2; i++) {
+            for (let j = -1; j < 2; j++) {
+                const xNow = x + i
+                const yNow = y + j
+                if (xNow >= 0 && xNow < 8 && yNow >= 0 && yNow < 8) {
+                    if (!game[xNow][yNow].bord) {
+                        game[xNow][yNow].box.dataset.dot = 1
+                        nextMove.push({
+                            x : xNow,
+                            y : yNow,
+                        })
+                    } else if (game[xNow][yNow].player !== game[x][y].player) {
+                        game[xNow][yNow].box.dataset.dot = 2
+                        nextMove.push({
+                            x : xNow,
+                            y : yNow,
+                        })
+                    }
+                }
+
+            }
+        }
     }
 }
 
@@ -213,4 +319,170 @@ function move(x, y) {
     oldBoard.innerText = ''
     oldBoard.dataset.player = ''
 
+}
+
+function bishopMove(x, y) {
+    let plusPlus = false
+    let plusMinus = false
+    let minusPlus = false
+    let minusMinus = false
+
+    for (let i = 1; i < 8; i++) {
+        if (x + i >= 0 && x + i < 8 && y + i >= 0 && y + i < 8) {
+            if (!game[x + i][y + i].bord && !plusPlus) {
+                game[x + i][y + i].box.dataset.dot = 1
+                nextMove.push({
+                    x : x + i,
+                    y : y + i,
+                })
+            } else if (game[x + i][y + i].player !== game[x][y].player && !plusPlus) {
+                game[x + i][y + i].box.dataset.dot = 2
+                nextMove.push({
+                    x : x + i,
+                    y : y + i,
+                })
+                plusPlus = true
+            } else {
+                plusPlus = true
+            }
+        }
+        if (x + i >= 0 && x + i < 8 && y - i >= 0 && y - i < 8) {
+            if (!game[x + i][y - i].bord && !plusMinus) {
+                game[x + i][y - i].box.dataset.dot = 1
+                nextMove.push({
+                    x : x + i,
+                    y : y - i,
+                })
+            } else if (game[x + i][y - i].player !== game[x][y].player && !plusMinus) {
+                game[x + i][y - i].box.dataset.dot = 2
+                nextMove.push({
+                    x : x + i,
+                    y : y - i,
+                })
+                plusMinus = true
+            } else {
+                plusMinus = true
+            }
+        }
+        if (x - i >= 0 && x - i < 8 && y + i >= 0 && y + i < 8) {
+            if (!game[x - i][y + i].bord && !minusPlus) {
+                game[x - i][y + i].box.dataset.dot = 1
+                nextMove.push({
+                    x : x - i,
+                    y : y + i,
+                })
+            } else if (game[x - i][y + i].player !== game[x][y].player && !minusPlus) {
+                game[x - i][y + i].box.dataset.dot = 2
+                nextMove.push({
+                    x : x - i,
+                    y : y + i,
+                })
+                minusPlus = true
+            } else {
+                minusPlus = true
+            }
+        }
+        if (x - i >= 0 && x - i < 8 && y - i >= 0 && y - i < 8) {
+            if (!game[x - i][y - i].bord && !minusMinus) {
+                game[x - i][y - i].box.dataset.dot = 1
+                nextMove.push({
+                    x : x - i,
+                    y : y - i,
+                })
+            } else if (game[x - i][y - i].player !== game[x][y].player && !minusMinus) {
+                game[x - i][y - i].box.dataset.dot = 2
+                nextMove.push({
+                    x : x - i,
+                    y : y - i,
+                })
+                minusMinus = true
+            } else {
+                minusMinus = true
+            }
+        }
+    }
+}
+
+function rookMove(x, y) {
+    let axisXMin = false
+    let axisXPlus = false
+    let axisYMin = false
+    let axisYPlus = false
+
+    for (let i = 1; i < 8; i++) {
+        if (x + i < 8) {
+            if (!game[x + i][y].bord && !axisXPlus) {
+                game[x + i][y].box.dataset.dot = 1
+                nextMove.push({
+                    x : x + i,
+                    y : y,
+                })
+            } else if (game[x + i][y].player !== game[x][y].player && !axisXPlus) {
+                game[x + i][y].box.dataset.dot = 2
+                nextMove.push({
+                    x : x + i,
+                    y : y,
+                })
+                axisXPlus = true
+            } else {
+                axisXPlus = true
+            }
+            
+        }
+        if (x - i >= 0) {
+            if (!game[x - i][y].bord && !axisXMin) {
+                game[x - i][y].box.dataset.dot = 1
+                nextMove.push({
+                    x : x - i,
+                    y : y,
+                })
+            } else if (game[x - i][y].player !== game[x][y].player && !axisXMin) {
+                game[x - i][y].box.dataset.dot = 2
+                nextMove.push({
+                    x : x - i,
+                    y : y,
+                })
+                axisXMin = true
+            } else {
+                axisXMin = true
+            }
+        }
+        if (y + i < 8) {
+            if (!game[x][y + i].bord && !axisYPlus) {
+                game[x][y + i].box.dataset.dot = 1
+                nextMove.push({
+                    x : x,
+                    y : y + i,
+                })
+            } else if (game[x][y + i].player !== game[x][y].player && !axisYPlus) {
+                game[x][y + i].box.dataset.dot = 2
+                nextMove.push({
+                    x : x,
+                    y : y + i,
+                })
+                axisYPlus = true
+            } else {
+                axisYPlus = true
+            }
+            
+        }
+        if (y - i >= 0) {
+            if (!game[x][y - i].bord && !axisYMin) {
+                game[x][y - i].box.dataset.dot = 1
+                nextMove.push({
+                    x : x,
+                    y : y - i,
+                })
+            } else if (game[x][y - i].player !== game[x][y].player && !axisYMin) {
+                game[x][y - i].box.dataset.dot = 2
+                nextMove.push({
+                    x : x,
+                    y : y - i,
+                })
+                axisYMin = true
+            } else {
+                axisYMin = true
+            }
+        }
+    }
 }
