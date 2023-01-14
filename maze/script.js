6c5ff86d4cf9579cae6e69d9b2e4e48dfa205b53
arxs-abd <70242781+arxs-abd@users.noreply.github.com>
2023-01-15 03:48:52 +0800

    update cant create and solve maze

--- maze/script.js
+++ maze/script.js
@@ -1,21 +1,18 @@
 const container = document.querySelector('.container')
 const containerBox = document.querySelector('.box-container')
 
-const row = 40
-const col = 60
-// const row = 20
-// const col = 30
+const row = 39
+const col = 61
+// const row = 101
+// const col = 101
 const path = []
 const grid = []
+const frontier = []
+
 let wall = 800
+
 let start
 let end
-
-// const spot = {
-//     f : 0, 
-//     g : 0, // Jarak
-//     h: 0,
-// }
 
 let openSet = []
 let closedSet = []
@@ -30,74 +27,82 @@ for (let i = 0; i < row; i++) {
         box.classList.add('box')
         box.dataset.i = i
         box.dataset.j = j
+        box.style.backgroundColor = 'black'
         rowBox.appendChild(box)
         rowGrid.push({
             box,
-            i : i,
-            j : j,
+            i,
+            j,
             f : 0,
             g : 0,
             h : 0,
-            isWall : false,
+            isWall : true,
             previous : undefined,
             neighbors : undefined
         })
-        // box.addEventListener('click', function() {
-        //     // console.log({...grid[i][j]})
-        //     box.style.backgroundColor = 'black'
-        //     grid[i][j].isWall = true
-        // })
     }
     containerBox.appendChild(rowBox)
     grid.push(rowGrid)
 }
-// const allBox = document.querySelectorAll('.box')
-
-// allBox.forEach(box => {
-//     box.addEventListener('click', function() {
-//         const {i, j} = box.dataset
-//         box.style.backgroundColor = 'black'
-//         grid[i][j].isWall = true
-//     })
-// })
-
-start = grid[getRandom(0, row - 1)][getRandom(0, col - 1)]
-end = grid[getRandom(0, row - 1)][getRandom(0, col - 1)]
-// start = grid[0][0]
-// end = grid[row - 1][col - 1]
-
-start.box.innerText = 'S'
-start.box.style.color = 'blue'
-end.box.innerText = 'F'
-end.box.style.color = 'blue'
-// Kasih Rintangan
-
-const tempWall = []
-while (tempWall.length < wall) {
-    const x = getRandom(0, row - 1)
-    const y = getRandom(0, col - 1)
-
-    if (!grid[x][y].isWall) {
-        grid[x][y].isWall = true
-        tempWall.push('*')
-    }
-}
-
-start.isWall = false
-end.isWall = false
-
-for (let i = 0; i < row; i++) {
-    for (let j = 0; j < col; j++) {
-        if (grid[i][j].isWall) grid[i][j].box.style.backgroundColor = 'black'
-        grid[i][j].neighbors = getNeighbors(grid[i][j])
-    }
-}
 
 document.addEventListener('keypress', function(e) {
-    if (e.key === 'Enter') run()
+    // console.log(e.key)
+    if (e.key === 'Enter') generateMaze()
+    else if (e.key === ' ') solveMaze()
 })
 
-async function run() {
+
+async function generateMaze() {
+    let x = 0
+    let y = 0
+    
+    grid[x][y].isWall = false
+    grid[x][y].box.style.backgroundColor = ''
+    
+    frontier.push(...getNeighbor(grid[x][y], true))
+
+    while (frontier.length > 0) {
+
+        const frontierCell = frontier[getRandom(0, frontier.length - 1)]
+        const neighbor = getNeighbor(frontierCell, false)
+    
+        if (neighbor.length > 0) {
+            const randNeighbor = neighbor[getRandom(0, neighbor.length - 1)]
+    
+            const xx = (randNeighbor.i + frontierCell.i) / 2
+            const yy = (randNeighbor.j + frontierCell.j) / 2
+    
+            frontierCell.isWall = false
+            grid[xx][yy].isWall = false
+            randNeighbor.isWall = false
+    
+            frontierCell.box.style.backgroundColor = ''
+            grid[xx][yy].box.style.backgroundColor = ''
+            randNeighbor.box.style.backgroundColor = ''
+        }
+        const addFrontier = getNeighbor(frontierCell, true)
+        // console.log(addFrontier)
+        removeFromArray(frontier, frontierCell)
+        frontier.push(...addFrontier)
+        // console.log(frontier)
+        // if (numberCount === 1000) break
+        // else numberCount++
+        await sleep(10)
+    }
+
+    console.log('selesai Membuat Maze')
+}
+
+async function solveMaze() {
+    for (let i = 0; i < row; i++) {
+        for (let j = 0; j < col; j++) {
+            if (grid[i][j].isWall) grid[i][j].box.style.backgroundColor = 'black'
+            grid[i][j].neighbors = getNeighbors(grid[i][j])
+        }
+    }
+
+    start = grid[0][0]
+    end = grid[row - 1][col - 1]
     
     openSet.push(start)
     while (openSet.length > 0) {
@@ -124,7 +129,7 @@ async function run() {
                 element.box.style.backgroundColor = 'yellow'
             })
     
-            console.log('Selesai')
+            console.log('Selesai Mencari Jalan')
             break
         }
     
@@ -173,14 +178,22 @@ async function run() {
     }
 }
 
-function removeFromArray(arr, elm) {
-    // return arr.filter(function(a) {
-    //     a.i !== elm.i && a.j !== elm.y
-    // })
+function getNeighbor(cell, isWall) {
+    const result = []
+    const x = cell.i
+    const y = cell.j
+    const possible = [[x - 2, y], [x, y - 2], [x + 2, y], [x, y + 2]]
+    // const possible = [[x - 1, y], [x, y - 1], [x + 1, y], [x, y + 1]]
 
-    for (let i = arr.length - 1; i >= 0; i--) {
-        if (arr[i] === elm) arr.splice(i, 1)
+
+    for (let i = 0; i < possible.length; i++) {
+        const newX = possible[i][0]
+        const newY = possible[i][1]
+        if (newX >= 0 && newX < row && newY >= 0 && newY < col && grid[newX][newY].isWall === isWall) {
+            result.push(grid[newX][newY])
+        }
     }
+    return result
 }
 
 function getNeighbors(current) {
@@ -200,17 +213,23 @@ function getNeighbors(current) {
     return result
 }
 
-function heuresticDistance(from, to) {
-    // return Math.abs(from.i - to.i) + Math.abs(from.j - to.j)
-    const x = Math.pow(to.i - from.i)
-    const y = Math.pow(to.j - from.j)
-    return Math.sqrt(x + y)
-}
-
 function getRandom(min, max) {
     return Math.round(Math.random() * (max - min) + min)
 }
 
 function sleep(time) {
     return new Promise(resolve => setTimeout(resolve, time))
+}
+
+function removeFromArray(arr, elm) {
+    for (let i = arr.length - 1; i >= 0; i--) {
+        if (arr[i].i === elm.i && arr[i].j === elm.j) arr.splice(i, 1)
+    }
+}
+
+function heuresticDistance(from, to) {
+    // return Math.abs(from.i - to.i) + Math.abs(from.j - to.j)
+    const x = Math.pow(to.i - from.i)
+    const y = Math.pow(to.j - from.j)
+    return Math.sqrt(x + y)
 }
