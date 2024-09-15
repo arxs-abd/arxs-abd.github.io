4743d215a5edb751ed31b5095405338d88446c62
= <=>
2024-09-16 00:14:43 +0800

    update : update parkee lagi

---
+++ no-parkee/minimarket.js
@@ -0,0 +1,110 @@
+const DATAS = [
+    // INDOMARET
+    {
+        "name" : "Tamangapa D'Mansion",
+        "type" : "Indomaret",
+        "lat" : -5.1901095351362105,
+        "long" : 119.49271917048138
+    },
+    {
+        "name" : "Tamangapa 283",
+        "type" : "Indomaret",
+        "lat" : -5.187321404706767,
+        "long" : 119.491652901271
+    },
+    {
+        "name" : "Tamangapa Raya",
+        "type" : "Indomaret",
+        "lat" : -5.1839154025332785,
+        "long" : 119.49027748063489
+    },
+    {
+        "name" : "Tamangapa 115",
+        "type" : "Indomaret",
+        "lat" : -5.179870007564158,
+        "long" : 119.4875419225195
+    },
+    {
+        "name" : "Tamangapa",
+        "type" : "Indomaret",
+        "lat" : -5.170946886840916,
+        "long" : 119.47966643784132
+    },
+    {
+        "name" : "Perumnas Antang",
+        "type" : "Indomaret",
+        "lat" : -5.167282089592902,
+        "long" : 119.48281176447354
+    },
+    {
+        "name" : "Antang Raya 90",
+        "type" : "Indomaret",
+        "lat" : -5.165105701141663,
+        "long" : 119.47733952079453
+    },
+    // ALFAMART
+    {
+        "name" : "Tamangapa D'Mansion",
+        "type" : "Alfamart",
+        "lat" : -5.1895944374654555,
+        "long" : 119.49244867398842
+    },
+    {
+        "name" : "Tamangapa Raya 3",
+        "type" : "Alfamart",
+        "lat" : -5.182259977298772,
+        "long" : 119.48799590154326
+    },
+    {
+        "name" : "Tamangapa Manggala",
+        "type" : "Alfamart",
+        "lat" : -5.175978962044222,
+        "long" : 119.48232859397712
+    },
+    {
+        "name" : "Tamangapa (R053)",
+        "type" : "Alfamart",
+        "lat" : -5.172371032413239,
+        "long" : 119.48004004580866
+    },
+    // ALFAMIDI
+    {
+        "name" : "Bangkala Tamangapa Raya 2",
+        "type" : "Alfamidi",
+        "lat" : -5.187085587322779,
+        "long" : 119.49163712689882
+    },
+    {
+        "name" : "Tamangapa Raya (ME16)",
+        "type" : "Alfamidi",
+        "lat" : -5.171056844645963,
+        "long" : 119.48002937374122
+    },
+]
+
+// Fungsi untuk menghitung jarak antara dua koordinat menggunakan rumus Haversine
+function calculateDistance(lat1, lon1, lat2, lon2) {
+    const R = 6371
+    const dLat = (lat2 - lat1) * (Math.PI / 180)
+    const dLon = (lon2 - lon1) * (Math.PI / 180)
+    const a = 
+        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.cos(lat1 * (Math.PI / 180)) * Math.cos(lat2 * (Math.PI / 180)) * 
+        Math.sin(dLon / 2) * Math.sin(dLon / 2)
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+    const distance = R * c
+    return distance
+}
+
+// Fungsi untuk menghitung jarak dari setiap data dalam array DATAS
+function calculateDistancesFrom(lat, lon, dataList) {
+    return dataList.map(location => {
+        const distance = calculateDistance(lat, lon, location.lat, location.long)
+
+        // console.log(`Jarak ${location.type}: ${location.name}: ${distance.toFixed(2)} km`)
+        return {
+            ...location,
+            distance
+        }
+    })
+}
--- no-parkee/script.js
+++ no-parkee/script.js
@@ -3,32 +3,68 @@ const map = L.map('map').setView([0, 0], 13);
 
 // Add a tile layer (using OpenStreetMap tiles)
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  // attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
 // Function to handle success when geolocation is found
 function onLocationFound(e) {
-  const latlng = e.latlng;
-  console.log(latlng)
+  const latlng = e.latlng
 
   // Create a marker at the user's location
-  const marker = L.marker([latlng.lat, latlng.lng]).addTo(map);
+  const marker = L.marker([latlng.lat, latlng.lng]).addTo(map)
 
   // Add popup with location details
-  marker.bindPopup(`You are here: <br> Latitude: ${latlng.lat}, Longitude: ${latlng.lng}`).openPopup();
+  marker.bindPopup(`Kamu Disini`)
 
   // Set the map view to user's location
-  map.setView([latlng.lat, latlng.lng], 13);
-  
-  const circleMarker = L.circleMarker([-6.200000, 106.816666], {
-    radius: 8,        // Size of the dot
-    color: '#ff0000', // Border color of the dot
-    fillColor: '#ff6666', // Fill color of the dot
-    fillOpacity: 0.9  // Opacity of the fill
-  }).addTo(map);
+  map.setView([latlng.lat, latlng.lng], 13)
 
-  // Add popup to the circle marker
-  circleMarker.bindPopup('<b>Jakarta</b><br>This is a dot marker.').openPopup();
+  // Fungsi untuk membuat legenda
+  const legend = L.control({ position: 'bottomright' })
+
+  legend.onAdd = function (map) {
+    const div = L.DomUtil.create('div', 'legend');
+    div.innerHTML += '<i style="background: #2981ca"></i> Indomaret<br>'
+    div.innerHTML += '<i style="background: #d7172e"></i> Alfamart<br>'
+    div.innerHTML += '<i style="background: #340308"></i> Alfamidi<br>'
+    return div
+  };
+
+  legend.addTo(map)
+
+  // for (const data of DATAS) {
+  for (const data of calculateDistancesFrom(latlng.lat, latlng.lng, DATAS)) {
+    let color = '#2981ca' // Default Indomaret
+    if (data.type === 'Alfamart') color = '#d7172e' // Alfamart 
+    else if (data.type === 'Alfamidi') color = '#340308' // Alfamidi
+
+    const circleMarker = L.circleMarker([data.lat, data.long], {
+      radius: 6,        
+      color: color,
+      fillColor: color, 
+      fillOpacity: 1
+    }).addTo(map);
+  
+    // Add popup to the circle marker
+    circleMarker.bindPopup(`<b>${data.type}</b><br>${data.name}. <a href="https://www.google.com/maps/dir/?api=1&destination=${data.lat},${data.long}" target="_blank">Lihat Rute</a>`);
+  }
+  
+
+  let markerx = null
+
+  map.on('click', function(e) {
+    const lat = e.latlng.lat
+    const lng = e.latlng.lng
+
+    if (markerx) map.removeLayer(markerx)
+
+    // Log koordinat yang diklik
+    console.log(`Koordinat yang diklik: Latitude = ${lat}, Longitude = ${lng}`)
+
+    // Tambahkan marker di lokasi yang diklik
+    // markerx = L.marker([lat, lng]).addTo(map)
+    //   .bindPopup(`Latitude: ${lat}, Longitude: ${lng}`).openPopup();
+  });
 
 }
 
