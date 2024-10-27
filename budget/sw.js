befa6f8a9912d665521109b30331ba89506863cf
= <=>
2024-10-28 00:18:05 +0800

    update service worker

--- budget/sw.js
+++ budget/sw.js
@@ -1,10 +1,10 @@
 const app = "budget-tracker-v1";
 const assets = [
-  "/",
-  "/index.html",
-  "/transaction.html",
-  "/style.css",
-  "/main.js",
+  "/budget",
+  "/budget/index.html",
+  "/budget/transaction.html",
+  "/budget/style.css",
+  "/budget/main.js",
   "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css",
   "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css",
   "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js",
