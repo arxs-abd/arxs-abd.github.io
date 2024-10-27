af44f00498f80788b6162f313df9c45648f1cd04
= <=>
2024-10-27 13:31:02 +0800

    add cahce

--- budget/sw.js
+++ budget/sw.js
@@ -2,8 +2,14 @@ const app = "budget-tracker-v1";
 const assets = [
   "/",
   "/index.html",
+  "/transaction.html",
   "/style.css",
   "/main.js",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css",
+  "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js",
+  "https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
 ];
 
 self.addEventListener("install", installEvent => {
