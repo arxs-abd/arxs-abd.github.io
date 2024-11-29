bd21a60fa7e069d5aeae2ad136a988186007effa
= <=>
2024-11-29 18:47:39 +0800

    update sc app again

--- sc-app/script.js
+++ sc-app/script.js
@@ -1,13 +1,26 @@
 // VARIABLES
-let score = {
-  home: 0,
-  away: 0
+let SETTINGS = {
+  game : {
+    increment: 1,
+  },
+  home : {
+    name: 'Player 1',
+    color : '#2196f3',
+    score: 0,
+  },
+  away : {
+    name: 'Player 2',
+    color : '#f44235',
+    score: 0,
+  }
 }
+
 let startY = 0
 let endY = 0
 
 // SELECTOR
 const settingsButton = document.querySelector('.setting')
+const closeSettingsButton = document.querySelector('#close-settings')
 const settingsPanel = document.querySelector('#settings-panel')
 const homeTeam = document.querySelector('#home')
 const awayTeam = document.querySelector('#away')
@@ -16,44 +29,47 @@ const awayTeamScore = awayTeam.querySelector('.team-score')
 
 // LISTENERS
 homeTeam.addEventListener('click', function() {
-  score.home += 1
+  updateScore('home', true)
   updateScore()
 })
 
 awayTeam.addEventListener('click', function() {
-  score.away += 1
+  updateScore('away', true)
   updateScore()
 })
 
-homeTeam.addEventListener('touchstart', (e) => {
-  startY = e.touches[0].clientY; // Ambil posisi Y saat touch dimulai
-})
-
-homeTeam.addEventListener("touchmove", (e) => {
-  endY = e.touches[0].clientY; // Update posisi Y saat swipe bergerak
-})
-
-homeTeam.addEventListener('touchend', (e) => {
-  if (startY < endY && endY - startY > 50) { // jika swipe ke bawah
-    score.home -= 1
-    updateScore()
-  }
-})
-
-
-
-
+detectSwipe(homeTeam, 'home')
+detectSwipe(awayTeam, 'away')
 
 settingsButton.addEventListener('click', function() {
   document.getElementById('settings-panel').style.bottom = '0'
 })
   
-settingsPanel.addEventListener('click', function() {
+closeSettingsButton.addEventListener('click', function() {
   document.getElementById('settings-panel').style.bottom = '-100%'
 })
 
 // FUNCTIONS
-function updateScore() {
-  homeTeamScore.innerHTML = score.home
-  awayTeamScore.innerHTML = score.away
+function updateScore(type, isPlus) {
+  if (type === 'home') SETTINGS.home.score += isPlus ? SETTINGS.game.increment : SETTINGS.game.increment * -1
+  else if (type === 'away') SETTINGS.away.score += isPlus ? SETTINGS.game.increment : SETTINGS.game.increment * -1
+  homeTeamScore.innerHTML = SETTINGS.home.score
+  awayTeamScore.innerHTML = SETTINGS.away.score
+}
+
+function detectSwipe(selector, type) {
+  selector.addEventListener('touchstart', (e) => {
+    startY = e.touches[0].clientY
+  })
+  
+  selector.addEventListener("touchmove", (e) => {
+    endY = e.touches[0].clientY
+  })
+  
+  selector.addEventListener('touchend', (e) => {
+    if (startY < endY && endY - startY > 50) {
+      updateScore(type, false)
+      updateScore()
+    }
+  })
 }
