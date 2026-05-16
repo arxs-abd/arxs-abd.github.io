8511a3772415a2a861e74ae8d86ae92824349f03
Aris Akhyar <aris.akhyar@binerteknologi.id>
2026-05-17 06:04:05 +0800

    update : adjust path

--- ab/service-worker.js
+++ ab/service-worker.js
@@ -1,8 +1,11 @@
-const CACHE_NAME = 'absensi-pwa-v1';
+const CACHE_NAME = 'absensi-pwa-v2-1.0';
 
-const urlsToCache = ['./', './index.html', './logo.jpeg', './manifest.json', 'https://cdn.tailwindcss.com', 'https://unpkg.com/html5-qrcode'];
+const urlsToCache = ['/ab/', '/ab/index.html', '/ab/manifest.json', '/ab/logo.jpeg', 'https://cdn.tailwindcss.com', 'https://unpkg.com/html5-qrcode'];
 
+// INSTALL
 self.addEventListener('install', (event) => {
+  self.skipWaiting();
+
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(urlsToCache);
@@ -10,10 +13,33 @@ self.addEventListener('install', (event) => {
   );
 });
 
+// ACTIVATE
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
+// FETCH
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+      return (
+        response ||
+        fetch(event.request).catch(() => {
+          return caches.match('./index.html');
+        })
+      );
     })
   );
 });
